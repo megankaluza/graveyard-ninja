19c5b239d78fdedcbd8c83d1e39c7d245fd28812
Caleb Paul and Megan Kaluza <caleb@>
2016-08-29 11:57:27 -0700

    add collision detection to js file, and color change to css file

--- js/scripts.js
+++ js/scripts.js
@@ -7,23 +7,44 @@
     window.requestAnimationFrame = requestAnimationFrame;
   })();
 
-  var canvas = document.getElementById("canvas"),
-      ctx = canvas.getContext("2d"),
-      width = 800,
-      height = 600,
-      player = {
-        x : width/2,
-        y : height - 5,
-        width : 5,
-        height : 5,
-        speed: 3,
-        velX: 0,
-        velY: 0,
-        jumping: false
-      },
-      keys = [],
-      friction = 0.8,
-      gravity = 0.3;
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"),
+    width = 800,
+    height = 600,
+    player = {
+      x : width/2,
+      y : height - 5,
+      width : 5,
+      height : 5,
+      speed: 3,
+      velX: 0,
+      velY: 0,
+      jumping: false
+    },
+    keys = [],
+    friction = 0.8,
+    gravity = 0.3;
+
+var boxes = []
+//dimensions
+boxes.push({
+  x: 0,
+  y:0,
+  width: 10,
+  height: height
+});
+boxes.push({
+  x: 0,
+  y: height - 2,
+  width: width,
+  height: 50
+});
+boxes.push({
+  x: width - 10,
+  y: 0,
+  width: 50,
+  height: height
+});
 
 canvas.width = width;
 canvas.height = height;
@@ -67,10 +88,45 @@ function update(){
   player.velY += gravity;
 
   ctx.clearRect(0, 0, width, height);
+  ctx.fillStyle = "black";
+  ctx.beginPath();
+
+  for(var i = 0; i < boxes.length; i++) {
+    ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
+  }
+  ctx.fill();
+
   ctx.fillStyle = "pink";
   ctx.fillRect(player.x, player.y, player.width, player.height);
   requestAnimationFrame(update);
 };
+
+function colCheck(shapeA, shapeB) {
+  var vX = (shapeA.x + (shapeA.width / 2)) - (shapeB.x + (shapeB.width / 2)),
+      vY = (shapeA.y +(shapeA.height / 2)) - (shapeB.y + (shapeB.height / 2)),
+      hWidths = (shapeA.width / 2) + (shapeB.width / 2),
+      hHeights = (shapeA.height / 2) + (shapeB.height / 2),
+      colDir = null;
+
+  if (Math.abs(vX) < hWidths && Math.abs(vY) < hHeights) {
+    if (vY > 0) {
+      colDir = "t";
+      shapeA.y += oY;
+    } else {
+      colDir = "b";
+      shapeA.y -= oY;
+    }
+  } else {
+    if (vX > 0) {
+      colDir = "l";
+      shapeA.x += oX;
+    } else {
+      colDir = "r";
+      shapeA.x -+ oX;
+    }
+  }
+  return colDir;
+}
 
 document.body.addEventListener("keydown", function(e){
   keys[e.keyCode] = true;
